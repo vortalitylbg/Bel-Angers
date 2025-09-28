628e98138f3f09564f0517abbbaa4cd4b876f069
Palacraft <99590109+vortalitylbg@users.noreply.github.com>
2025-09-28 13:19:55 +0200

    Delete jsx directory

--- jsx/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import LiquidNavbar from './liquidNavbar.jsx'
-
-const rootElement = document.getElementById('liquid-navbar')
-if (rootElement) {
-  const root = createRoot(rootElement)
-  root.render(<LiquidNavbar />)
-}
